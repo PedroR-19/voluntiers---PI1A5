df8d56e952d4e0adc824bfc6b695a18a2dfc705b
PedroR-19 <p.scianni@aluno.ifsp.edu.br>
2024-06-17 12:38:45 -0300

    ok until here

--- profiles/forms/register_form.py
+++ profiles/forms/register_form.py
@@ -2,72 +2,70 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from .django_forms import add_placeholder, strong_password
-
+from django.utils.translation import gettext_lazy as _
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_placeholder(self.fields['user_type'], 'ONG ou Voluntier')
-        add_placeholder(self.fields['username'], 'Seu nome de usuário')
-        add_placeholder(self.fields['email'], 'Seu e-mail')
-        add_placeholder(self.fields['first_name'], 'Ex.: Pedro')
-        add_placeholder(self.fields['last_name'], 'Ex.: Costa')
-        add_placeholder(self.fields['password'], 'Digite sua senha')
-        add_placeholder(self.fields['password2'], 'Repita sua senha')
+        add_placeholder(self.fields['user_type'], 'ONG or Voluntier')
+        add_placeholder(self.fields['username'], _('Your name'))
+        add_placeholder(self.fields['email'], _('Your e-mail'))
+        add_placeholder(self.fields['first_name'], _('Ex.: Pedro'))
+        add_placeholder(self.fields['last_name'], _('Ex.: Costa'))
+        add_placeholder(self.fields['password'], _('Write your password'))
+        add_placeholder(self.fields['password2'], _('Write your password again'))
 
     user_type = forms.ChoiceField(
-    choices=[
-        ('ONG', 'ONG'),
-        ('Voluntier', 'Voluntier'),
-        
-    ],
-    error_messages={'required': 'Please select an option'},
-    label='User_type'
+        choices=[
+            ('ONG', 'ONG'),
+            ('Voluntier', 'Voluntier'),
+        ],
+        error_messages={'required': _('Please select an option')},
+        label=_('User_type')
     )
     username = forms.CharField(
-        label='Username',
-        help_text=(
-            'Nome de usuário deve conter letras, números ou um destes: @.+-_. '
-            'Nome de usuário deve conter entre 4 e 150 caracteres.'
+        label=_('Username'),
+        help_text=_(
+            _('Username must contain letters, numbers, or one of these: @.+-_. Username must contain between 4 and 150 characters.')
         ),
         error_messages={
-            'required': 'This field must not be empty',
-            'min_length': 'Username must have at least 4 characters',
-            'max_length': 'Username must have less than 150 characters',
+            'required': _('This field must not be empty'),
+            'min_length': _('Username must have at least 4 characters'),
+            'max_length': _('Username must have less than 150 characters'),
         },
         min_length=4, max_length=150,
     )
     first_name = forms.CharField(
-        error_messages={'required': 'Write your first name'},
-        label='First name'
+        error_messages={'required': _('Write your first name')},
+        label=_('First name')
     )
     last_name = forms.CharField(
-        error_messages={'required': 'Write your last name'},
-        label='Last name'
+        error_messages={'required': _('Write your last name')},
+        label=_('Last name')
     )
     email = forms.EmailField(
-        error_messages={'required': 'E-mail is required'},
+        error_messages={'required': _('E-mail is required')},
         label='E-mail',
-        help_text='O e-mail deve ser válido.',
+        help_text=_('The e-mail need to be valid.'),
     )
     password = forms.CharField(
         widget=forms.PasswordInput(),
         error_messages={
-            'required': 'Password must not be empty'
+            'required': _('Password must not be empty')
         },
-        help_text=(
-            'Senha deve conter ao menos uma letra maiúscula, '
-            'uma letra minúscula e um número.'
-            'ao menos 8 caracteres.'
+        help_text=_(
+            _('Password must contain at least one uppercase letter, '
+            'a lowercase letter and a number. '
+            'at least 8 characters.')
         ),
         validators=[strong_password],
-        label='Password'
+        label=_('Password')
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(),
-        label='Password2',
+        label=_('Password2'),
         error_messages={
-            'required': 'Please, repeat your password'
+            'required': _('Please, repeat your password')
         },
     )
 
@@ -88,7 +86,7 @@ class RegisterForm(forms.ModelForm):
 
         if exists:
             raise ValidationError(
-                'Este e-mail já está em uso.', code='invalid',
+                _('This email is already in use.'), code='invalid',
             )
 
         return email
@@ -101,7 +99,7 @@ class RegisterForm(forms.ModelForm):
 
         if password != password2:
             password_confirmation_error = ValidationError(
-                'Password e Password2 devem ser iguais',
+                _('Password and Password2 must be the same'),
                 code='invalid'
             )
             raise ValidationError({

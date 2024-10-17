42836e01e92b36be5ae9c30770743b0e47069a5c
PedroR-19 <102542541+PedroR-19@users.noreply.github.com>
2024-10-17 08:51:31 -0300

    strong password ok

--- profiles/forms/institution_form.py
+++ profiles/forms/institution_form.py
@@ -1,7 +1,22 @@
 from django                   import forms
 from django.utils.translation import gettext_lazy as _
-
 from profiles.models import Institution
+import re
+from django.core.exceptions import ValidationError
+
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
 
 
 class InstitutionForm(forms.ModelForm):
@@ -20,5 +35,7 @@ class InstitutionForm(forms.ModelForm):
 
         if password != confirm_password:
             raise forms.ValidationError('Passwords must match.')
-
+        
+        strong_password(password)
+        
         return cleaned_data

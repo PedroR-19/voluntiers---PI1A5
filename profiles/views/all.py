034fb0c5d78e4539a1f48f480c937734bf517c20
PedroR-19 <102542541+PedroR-19@users.noreply.github.com>
2024-11-06 22:06:55 -0300

    hiding match ( come back later)

--- profiles/views/all.py
+++ profiles/views/all.py
@@ -65,14 +65,14 @@ def match_view(request):
         if form.cleaned_data['shift']:
             positions = positions.filter(shift=form.cleaned_data['shift'])
 
-        if form.cleaned_data['state']:
-            positions = positions.filter(state=form.cleaned_data['state'])
+        # if form.cleaned_data['state']:
+        #     positions = positions.filter(state=form.cleaned_data['state'])
 
-        if form.cleaned_data['city']:
-            positions = positions.filter(city=form.cleaned_data['city'])
+        # if form.cleaned_data['city']:
+        #     positions = positions.filter(city=form.cleaned_data['city'])
 
-        if form.cleaned_data['zone']:
-            positions = positions.filter(zone=form.cleaned_data['zone'])
+        # if form.cleaned_data['zone']:
+        #     positions = positions.filter(zone=form.cleaned_data['zone'])
 
         if form.cleaned_data['category']:
             positions = positions.filter(category=form.cleaned_data['category'])

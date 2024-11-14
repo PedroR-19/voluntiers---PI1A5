f8f53320afd73c219ca5f61423699234d8c49deb
LeonardoTumani <nerdsregood@gmail.com>
2024-11-13 23:58:18 -0300

    Changed styles and added masks

--- base_static/global/js/masks.js
+++ base_static/global/js/masks.js
@@ -9,6 +9,16 @@ const masks = {
         // .replace(/[~^´`¨]/g, '');
       },
 
+    institution_name (value) {
+        return value
+        // .replace(/[^A-ü ]+/g, '')
+        .replace(/([A-ü ]{50})[A-ü ]+?$/, '$1')
+        .replace(/^ /g, '')
+        .replace(/(\s|^)[A-ü]/g, function(match) { return match.toUpperCase(); })
+        .replace(/ +/g, ' ')
+        // .replace(/[~^´`¨]/g, '');
+      },
+
     cpf (value) {
         return value
         .replace(/\D+/g, '')
@@ -18,13 +28,30 @@ const masks = {
         .replace(/(-\d{2})\d+?$/, '$1')
     },
 
-    cnpj (value) { //ATUALIZAR
+    cnpj (value) {
         return value
         .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-        .replace(/(-\d{2})\d+?$/, '$1')
+        .replace(/(\d{3})(\d{1,4})/, '$1/$2')
+        .replace(/(\/\d{4})(\d{1,2})/, '$1-$2')
+        .replace(/(\/\d{4}-\d{2})\d+?$/, '$1')
+    },
+
+    cep (value) {
+        return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+    },
+
+    about (value) {
+        return value
+        .replace(/[^A-ü ]+/g, '')
+        .replace(/([A-ü ]{300})[A-ü ]+?$/, '$1')
+        .replace(/^ /g, '')
+        // .replace(/(\s|^)[A-ü]/g, function(match) { return match.toUpperCase(); })
+        .replace(/ +/g, ' ')
     }
 
     // date (value) {
@@ -51,10 +78,54 @@ const masks = {
     // }
 }
 
-document.getElementById('id_cpf').forEach($input => {
-    const field = $input.dataset.js
-    
-    $input.addEventListener('input', e => {
-        e.target.value = masks[field](e.target.value)
+const id_cpf = document.getElementById('id_cpf')
+const id_cnpj = document.getElementById('id_cnpj')
+const id_cep = document.getElementById('id_cep')
+
+const id_first_name = document.getElementById('id_first_name')
+const id_last_name = document.getElementById('id_last_name')
+const id_name = document.getElementById('id_name')
+
+const id_about = document.getElementById('id_about')
+
+if (id_cpf) {
+    id_cpf.addEventListener('input', e => {
+        e.target.value = masks.cpf(e.target.value)
     }, false)
-})
+}
+
+if (id_cnpj) {
+    id_cnpj.addEventListener('input', e => {
+        e.target.value = masks.cnpj(e.target.value)
+    }, false)
+}
+
+if (id_cep) {
+    id_cep.addEventListener('input', e => {
+        e.target.value = masks.cep(e.target.value)
+    }, false)
+}
+
+if (id_first_name) {
+    id_first_name.addEventListener('input', e => {
+        e.target.value = masks.name(e.target.value)
+    }, false)
+}
+
+if (id_last_name) {
+    id_last_name.addEventListener('input', e => {
+        e.target.value = masks.name(e.target.value)
+    }, false)
+}
+
+if (id_name) {
+    id_name.addEventListener('input', e => {
+        e.target.value = masks.institution_name(e.target.value)
+    }, false)
+}
+
+if (id_about) {
+    id_about.addEventListener('input', e => {
+        e.target.value = masks.about(e.target.value)
+    }, false)
+}
